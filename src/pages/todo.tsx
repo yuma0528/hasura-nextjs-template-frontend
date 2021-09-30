47b060e54f75a2915f01e80bb8620051383bfd92
yuma <yumaito0528@gmail.com>
2021-10-01 03:30:30 +0900

    UI調整

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -4,6 +4,11 @@ import { useContext, useState } from 'react'
 import { AuthContext } from '../auth/AuthProvider'
 import { GET_TODOS} from '../repositories/queries';
 
+import { Button } from "@chakra-ui/button";
+import { AddIcon } from "@chakra-ui/icons";
+import { Input } from "@chakra-ui/input";
+import { Box, Flex } from "@chakra-ui/layout";
+import { List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react"
 export default function Home() {
   const [input, setInput] = useState<string>('')
 
@@ -18,24 +23,45 @@ export default function Home() {
   return (
     <div>
       <div>
-        { data?.todos.map( ( item: any ) => (
-          <div className="skel-single-post" key={ item.id }>{item.id} {item.title}</div>
-        ))}
+
       </div>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          addTodo({ variables: { user_id: user?.uid, title: input } })
-          setInput('')
-          refetch()
-        }}
-      >
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
+      <Box textAlign="center">
+        <UnorderedList spacing={3}>
+          { data?.todos.map( ( item: any ) => (
+            <ListItem>
+              {item.title}
+            </ListItem>
+          ))}
+        </UnorderedList>
+            <form
+              onSubmit={e => {
+                e.preventDefault()
+                addTodo({ variables: { user_id: user?.uid, title: input } })
+                setInput('')
+                refetch()
+              }}
+            >
+              <Input
+                mr={5}
+                bg="white"
+                placeholder="task"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+                <Button
+                  type="submit"
+                  colorScheme="yellow"
+                  variant="solid"
+                  cursor="pointer"
+                  leftIcon={<AddIcon />}
+
+                >
+                  Add Todo
+                </Button>
+            </form>
+
+      </Box>
     </div>
   )
 }
--- src/repositories/apollo.tsx
+++ src/repositories/apollo.tsx
@@ -1,14 +1,28 @@
 import { useContext, useEffect, useState, FC } from 'react'
 import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
 import { AuthContext } from '../auth/AuthProvider'
+var jwt = require('jsonwebtoken');
 
+const getLocallySignedToken = (token: string) => {
+  let decodeToken = jwt.decode(token)
+    decodeToken['https://hasura.io/jwt/claims'] =  {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": decodeToken.user_id,
+    }
+  return jwt.sign(decodeToken, "123456789012345678901234567890123");
+};
 
 const createApolloClient = (authToken: string | undefined) => {
+  let token = authToken
+  if (process.env.NODE_ENV !== "production" && authToken) {
+    token = getLocallySignedToken(authToken)
+  }
   return new ApolloClient({
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_SERVER_URL + '/graphql',
       headers: {
-        Authorization: `Bearer ${authToken}`
+        Authorization: `Bearer ${token}`
       }
     }),
     cache: new InMemoryCache(),
@@ -23,11 +37,9 @@ export const WithApolloProvider: FC = ({ children }) => {
   useEffect(() => {
     const fetchData = async () => {
       if (user) {
-        const token = await user.getIdToken()
         user.getIdToken().then(token => {
           setClient(createApolloClient(token))
           console.log('token2', token)
-
         })
       }
     }
